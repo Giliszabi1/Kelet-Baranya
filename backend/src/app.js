66fb498bc4964d2c19db845c3c678fa62716ee2c
Giliszabi1 <szabigameplay1@gmail.com>
2026-08-06 17:34:15 +0200

    KB-34 Rate-limit függőség konfigurációja

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const rateLimit = require('express-rate-limit');
 
 const config = require("./config/app.config");
 
@@ -11,6 +12,8 @@ app.use(express.json());
 const corsOption = config.corsOptions()
 app.use(cors(corsOption));
 
+const globalLimit = config.globalRateLimitOption()
+app.use(rateLimit(globalLimit))
 
 app.use("/", (req, res)=>{
     res.send("<h1>heéép</h1>")
--- backend/src/config/app.config.js
+++ backend/src/config/app.config.js
@@ -19,6 +19,22 @@ class AppConfig {
         }     
         return corsOption;
     }
+
+    globalRateLimitOption() {
+        const globalRateLimitConfig = {
+            windowMs: 15 * 60 * 1000, // 15 perc
+            limit: 100, // max 100 request / IP / ablak
+
+            message: {
+              error: "Too many requests, please try again later."
+            },
+
+            handler: (req, res, next, options) => {
+              res.status(options.statusCode).json(options.message);
+            }
+        }
+        return globalRateLimitConfig;
+    }
 }
 
 
